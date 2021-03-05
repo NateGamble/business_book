469c0e37c47f7b3472621ac010df1a3d693e9aa8
Jay Norman <jonathan.norman.park.alumni@gmail.com>
2021-03-05 15:54:21 -0600

    Added business repo and service. Haven't written a test for services.

---
+++ src/main/java/com/revature/repos/BusinessRepository.java
@@ -0,0 +1,21 @@
+package com.revature.repos;
+
+import com.revature.models.*;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BusinessRepository extends CrudRepository<Business, Integer> {
+    List<Business> findBusinessesByOwner(User owner);
+    Optional<Business> findBusinessByEmail(String email);
+    Optional<Business> findBusinessByBusinessName(String businessName);
+    Optional<Business> findBusinessByLocation(String location);
+    Optional<Business> findBusinessByRegisterDatetime(Timestamp registerDatetime);
+    List<BusinessReviews> findBusinessReviewsByBusiness(Business business);
+    List<BusinessHours> findBusinessHoursByBusiness(Business business);
+    List<Posts> findBusinessPostsByBusiness(Business business);
+}
