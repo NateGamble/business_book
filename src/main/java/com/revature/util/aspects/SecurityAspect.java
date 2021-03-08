869d74cad80e5affa627db3cda9ca19569a5ef2c
unknown <alex.googe@revature.net>
2021-03-08 16:06:31 -0500

    Added logic to SecurityAspect to use Jwt. Secured annotation needed to be for Methods not Type. JwtParser was giving a type conversion error. Fixed that in the parseToken method since claim.get was returning a String and not ENUM.

--- src/main/java/com/revature/util/aspects/SecurityAspect.java
+++ src/main/java/com/revature/util/aspects/SecurityAspect.java
@@ -3,13 +3,17 @@ package com.revature.util.aspects;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import com.revature.dtos.Principal;
 import com.revature.exceptions.AuthenticationException;
 import com.revature.exceptions.AuthorizationException;
+import com.revature.util.JwtConfig;
 import com.revature.util.Secured;
+import com.revature.util.JwtParser;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -17,13 +21,23 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 @Aspect
 public class SecurityAspect {
 
-    @Autowired HttpServletRequest request;
-    
+    private HttpServletRequest request;
+    private final JwtParser jwtParser;
+
+    @Autowired
+    public SecurityAspect(HttpServletRequest request, JwtParser jwtParser) {
+        this.request = request;
+        this.jwtParser = jwtParser;
+    }
+
+
     // Custom make exceptions
     @Around("@annotation(com.revature.util.Secured)")
     public Object secureEndpoint(ProceedingJoinPoint pjp) throws Throwable {
@@ -31,7 +45,24 @@ public class SecurityAspect {
         Secured securedAnno = method.getAnnotation(Secured.class);
 
         List<String> allowedRoles = Arrays.asList(securedAnno.allowedRoles());
-        Principal principal = (Principal) request.getSession().getAttribute("principal");
+
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null ) {
+            throw new AuthenticationException("An unauthenticated request was made to a protected endpoint!");
+        }
+
+        String token = Stream.of(cookies)
+                        .filter(c -> c.getName().equals("bb-token"))
+                        .findFirst()
+                        .orElseThrow(AuthenticationException::new)
+                        .getValue();
+
+        Principal principal = jwtParser.parseToken(token);
+
+        System.out.println(principal.getRole());
+
+        //Principal principal = (Principal) request.getAttribute("principal");
 
         if (principal == null) {
             throw new AuthenticationException();
