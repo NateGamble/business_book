9350d8ae0966519b1883fcec448dc82d8b037a98
Nate Gamble <nate.eli.gamble@gmail.com>
2021-03-08 11:18:49 -0500

    Changed swagger base package to correct one

--- src/main/java/com/revature/config/SwaggerConfig.java
+++ src/main/java/com/revature/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.revature.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.revature.web.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }
