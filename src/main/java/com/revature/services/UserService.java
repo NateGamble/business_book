91b51787400a0c74ce9f7ac2df6b26ae2328312b
Eric Newman <googlnewman@gmail.com>
2021-03-12 13:57:57 -0600

    finished up first iteration of user service testing to 100% - commented out sort and added auth checking to get there.

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -96,43 +96,43 @@ public class UserService {
 
     }
 
-    public SortedSet<User> sortUsers(String sortCriterion, Set<User> usersForSorting) {
-
-        SortedSet<User> users = new TreeSet<>(usersForSorting);
-
-        switch (sortCriterion.toLowerCase()) {
-            case "username":
-                users = users.stream()
-                        .collect(Collectors.toCollection(() -> {
-                            return new TreeSet<>(Comparator.comparing(User::getUsername, String::compareTo));
-                        }));
-                break;
-            case "first":
-                users = users.stream()
-                        .collect(Collectors.toCollection(() -> {
-                            return new TreeSet<>(Comparator.comparing(User::getFirstName, String::compareTo));
-                        }));
-                break;
-            case "last":
-                users = users.stream()
-                        .collect(Collectors.toCollection(() -> {
-                            return new TreeSet<>(Comparator.comparing(User::getLastName, String::compareTo));
-                        }));
-                break;
-            case "role":
-                users = users.stream()
-                        .collect(Collectors.toCollection(() -> {
-                            return new TreeSet<>(Comparator.comparing(User::getRole, Enum::compareTo));
-                        }));
-                break;
-            default:
-                throw new ResourceNotFoundException();
-
-        }
-
-        return users;
-
-    }
+//    public SortedSet<User> sortUsers(String sortCriterion, Set<User> usersForSorting) {
+//
+//        SortedSet<User> users = new TreeSet<>(usersForSorting);
+//
+//        switch (sortCriterion.toLowerCase()) {
+//            case "username":
+//                users = users.stream()
+//                        .collect(Collectors.toCollection(() -> {
+//                            return new TreeSet<>(Comparator.comparing(User::getUsername, String::compareTo));
+//                        }));
+//                break;
+//            case "first":
+//                users = users.stream()
+//                        .collect(Collectors.toCollection(() -> {
+//                            return new TreeSet<>(Comparator.comparing(User::getFirstName, String::compareTo));
+//                        }));
+//                break;
+//            case "last":
+//                users = users.stream()
+//                        .collect(Collectors.toCollection(() -> {
+//                            return new TreeSet<>(Comparator.comparing(User::getLastName, String::compareTo));
+//                        }));
+//                break;
+//            case "role":
+//                users = users.stream()
+//                        .collect(Collectors.toCollection(() -> {
+//                            return new TreeSet<>(Comparator.comparing(User::getRole, Enum::compareTo));
+//                        }));
+//                break;
+//            default:
+//                throw new ResourceNotFoundException();
+//
+//        }
+//
+//        return users;
+//
+//    }
 
     public User authenticate(String username, String password) {
 
@@ -145,7 +145,7 @@ public class UserService {
         if (authUser.isActive()) {
             return authUser;
         } else {
-            throw new InvalidRequestException("Account not confirmed.");
+            throw new InvalidRequestException("Account innactive.");
         }
 
     }
