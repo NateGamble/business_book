ef312afdc1897894de5097c5f4c20205b6b2d7cc
Brandon Johnson <brandon.johnson@revature.net>
2021-03-12 13:15:55 -0600

    added cors filter, and updated logging aspect to ignore filter

---
+++ src/main/java/com/revature/filters/CorsFilter.java
@@ -0,0 +1,24 @@
+package com.revature.filters;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@WebFilter("/*")
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.setHeader("Access-Control-Allow-Origin","*");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","Content-Type");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
