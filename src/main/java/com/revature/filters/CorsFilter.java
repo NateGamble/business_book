f48dfd4d46abc20e19745756c35679fed3e234de
Brandon Johnson <brandon.johnson@revature.net>
2021-03-15 09:08:05 -0500

    updated corFilter

---
+++ src/main/java/com/revature/filters/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.revature.filters;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@WebFilter("/*")
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.setHeader("Access-Control-Allow-Origin","http://localhost:8080");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://192.168.1.149:8080");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials","true");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","Content-Type");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
