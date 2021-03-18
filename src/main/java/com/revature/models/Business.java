aac6ab56397a828b0a19dc8f2398a8e64d494036
unknown <alex.googe@revature.net>
2021-03-18 10:29:11 -0400

    Finished JavaDocs for models.

--- src/main/java/com/revature/models/Business.java
+++ src/main/java/com/revature/models/Business.java
@@ -26,35 +26,65 @@ public class Business {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
+    /**
+     * The User Object for the Owner of the Business
+     */
     @ManyToOne(targetEntity = User.class, optional = false)
     @JoinColumn(name = "owner_id", columnDefinition = "int4 NOT NULL")
     private User owner;
 
+    /**
+     * The String for the email of the Business
+     */
     //@Pattern(regexp= RegexUtil.EMAIL_REGEX)
     @Column(nullable=false, unique=true)
     private String email;
 
+    /**
+     * The String value of the Business name
+     */
     @Column(name="business_name", nullable=false)
     private String businessName;
 
+    /**
+     * String for the address of the Business
+     */
     @Column(nullable=false)
     private String location;
 
+    /**
+     * String for the Business type
+     */
     @Column(name="business_type", nullable=false)
     private String businessType;
 
+    /**
+     * Timestamp for when the Business was created
+     */
     @Column(name="register_datetime", updatable=false, columnDefinition="timestamp default CURRENT_TIMESTAMP")
     private Timestamp registerDatetime;
 
+    /**
+     * Boolean for if the Business is "deleted" or not
+     */
     @Column(name="is_active")
     private boolean isActive;
 
+    /**
+     * List of Reviews for the Business
+     */
     @OneToMany(mappedBy = "business")
     List<Review> reviews;
 
+    /**
+     * List of Hours for the Business
+     */
     @OneToMany(mappedBy = "business")
     List<Hours> hours;
 
+    /**
+     * List of Posts for the Business
+     */
     @OneToMany(mappedBy = "business")
     List<Post> posts;
 
--- src/main/java/com/revature/models/Review.java
+++ src/main/java/com/revature/models/Review.java
@@ -15,27 +15,45 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Java POJO for a Review of a Business
+ */
 @Entity @Table(name = "business_reviews")
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Review {
 
+    /**
+     * Integer Id of the Review
+     */
     @Id @Column(name = "review_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    /**
+     * Business that the Review is for
+     */
     @JoinColumn(name = "business_id")
     @ManyToOne(targetEntity = Business.class, optional = false)
     @JsonIgnore
     private Business business;
 
+    /**
+     * User that created the Review
+     */
     @JoinColumn(name = "user_id")
     @ManyToOne(targetEntity = User.class, optional = false)
     @JsonIgnore
     private User user;
 
+    /**
+     * Rating provided by the User
+     */
     @Column(name = "rating", nullable = false)
     private Double rating;
 
+    /**
+     * String content of the Review
+     */
     @Column(name = "review")
     private String review;
     
